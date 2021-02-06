8c7b789d0fd79c643a13e9c9eaeba87dc7a3663a
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-02-06 11:41:09 +0200

    Creating Database Design

---
+++ Exams/28.Jun.2020/DatabaseDesign.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Planets
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Spaceports
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	PlanetId INT NOT NULL FOREIGN KEY REFERENCES Planets(Id)
+)
+
+CREATE TABLE Spaceships
+(
+	Id INT PRIMARY KEY IDENTITY(0,1),
+	[Name] VARCHAR(50) NOT NULL,
+	Manufacturer VARCHAR(30) NOT NULL,
+	LightSpeedRate INT DEFAULT(0)
+)
+
+CREATE TABLE Colonists
+(
+	Id INT PRIMARY KEY IDENTITY(0,1),
+	FirstName VARCHAR(20) NOT NULL,
+	LastName VARCHAR(20) NOT NULL,
+	Ucn VARCHAR(10) NOT NULL UNIQUE,
+	BirthDate DATE NOT NULL
+)
+
+CREATE TABLE Journeys
+(
+	Id INT PRIMARY KEY IDENTITY(0,1),
+	JourneyStart DATETIME NOT NULL,
+	JourneyEnd DATETIME NOT NULL,
+	Purpose VARCHAR(11),
+	DestinationSpaceportId INT NOT NULL FOREIGN KEY REFERENCES Spaceports(Id),
+	SpaceshipId INT NOT NULL FOREIGN KEY REFERENCES Spaceships(Id)
+)
+
+ALTER TABLE Journeys
+ADD CONSTRAINT df_Purpose
+CHECK (Purpose IN ( 'Medical', 'Technical', 'Educational', 'Military'))
+
+CREATE TABLE TravelCards
+(
+	Id INT PRIMARY KEY IDENTITY(0,1),
+	CardNumber CHAR(10) NOT NULL UNIQUE,
+	JobDuringJourney VARCHAR(8),
+	ColonistId INT NOT NULL FOREIGN KEY REFERENCES Colonists(Id),
+	JourneyId INT NOT NULL FOREIGN KEY REFERENCES Journeys(Id)
+)
+
+ALTER TABLE TravelCards
+ADD CONSTRAINT df_JobDuringJourney
+CHECK (JobDuringJourney IN ('Pilot', 'Engineer', 'Trooper', 'Cleaner', 'Cook'))
