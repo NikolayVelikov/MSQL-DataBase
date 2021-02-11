521b903982903c78fb7648d0de30b3600691dc3b
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-02-11 20:46:09 +0200

    Final revision of Available room querie

--- Exams/21.Jun.2020/11. Available Room.sql
+++ Exams/21.Jun.2020/11. Available Room.sql
@@ -1,20 +1,44 @@
 CREATE FUNCTION udf_GetAvailableRoom(@HotelId INT, @Date DATE, @People INT) 
-RETURNS NVARCHAR(MAX)				-- 112		  '2011-12-17'		2
+RETURNS NVARCHAR(MAX)				
 AS
 BEGIN
-
-	DECLARE @HotelBaseRate DECIMAL(19,2) = (SELECT BaseRate FROM Hotels WHERE Id = 112);
-	DECLARE @RoomPrice DECIMAL (19,2) = (SELECT r.Price FROM Rooms AS r JOIN Hotels AS h ON h.Id = r.HotelId WHERE h.Id = 112);
-	DECLARE @Price DECIMAL(15,2) = (@HotelBaseRate + @RoomPrice) * 2;
-	
-	SELECT *
+	DECLARE @RoomId INT = (SELECT k.Id FROM(SELECT TOP(1) r.Id,r.Type,r.Beds,(h.BaseRate + r.Price) * 2 AS RoomPrice
 		FROM Hotels AS h
 		JOIN Rooms AS r ON r.HotelId = h.Id
 		JOIN Trips AS t ON t.RoomId = r.Id
-		WHERE h.Id = 112 AND NOT('2011-12-17' BETWEEN t.ArrivalDate AND t.ReturnDate) AND r.Beds >= 2
+		WHERE h.Id = @HotelId AND NOT(@Date BETWEEN t.ArrivalDate AND t.ReturnDate) AND r.Beds >= @People
+	ORDER BY RoomPrice DESC) AS k);
 
+	DECLARE @RoomType NVARCHAR(20) = (SELECT k.Type FROM(SELECT TOP(1) r.Id,r.Type,r.Beds,(h.BaseRate + r.Price) * 2 AS RoomPrice
+		FROM Hotels AS h
+		JOIN Rooms AS r ON r.HotelId = h.Id
+		JOIN Trips AS t ON t.RoomId = r.Id
+		WHERE h.Id = @HotelId AND NOT(@Date BETWEEN t.ArrivalDate AND t.ReturnDate) AND r.Beds >= @People
+	ORDER BY RoomPrice DESC) AS k);
+
+	DECLARE @Beds INT = (SELECT k.Beds FROM(SELECT TOP(1) r.Id,r.Type,r.Beds,(h.BaseRate + r.Price) * 2 AS RoomPrice
+		FROM Hotels AS h
+		JOIN Rooms AS r ON r.HotelId = h.Id
+		JOIN Trips AS t ON t.RoomId = r.Id
+		WHERE h.Id = @HotelId AND NOT(@Date BETWEEN t.ArrivalDate AND t.ReturnDate) AND r.Beds >= @People
+	ORDER BY RoomPrice DESC) AS k);
+
+	DECLARE @Price DECIMAL(19,2) = (SELECT k.RoomPrice FROM(SELECT TOP(1) r.Id,r.Type,r.Beds,(h.BaseRate + r.Price) * 2 AS RoomPrice
+		FROM Hotels AS h
+		JOIN Rooms AS r ON r.HotelId = h.Id
+		JOIN Trips AS t ON t.RoomId = r.Id
+		WHERE h.Id = @HotelId AND NOT(@Date BETWEEN t.ArrivalDate AND t.ReturnDate) AND r.Beds >= @People
+	ORDER BY RoomPrice DESC) AS k);
+
+	DECLARE @Result NVARCHAR(MAX) = 'Room ' + CONVERT(NVARCHAR,@RoomId) + ': ' + CONVERT(NVARCHAR,@RoomType) 
+												+ ' (' + CONVERT(NVARCHAR,@Beds)+' beds) - $' + CONVERT(NVARCHAR,@Price)
+	IF(@Result IS NULL)
+		RETURN 'No rooms available'	
+		
+	RETURN @Result
+END
 	-- • ('2011-12-17' BETWEEN t.ArrivalDate AND t.ReturnDate) -> The room must not be already occupied. A room is occupied if the date the customers want to book is between the arrival and return dates of a trip to that room and the trip is not canceled.
 	-- • h.Id = 112 -> The room must be in the provided hotel.
 	-- • r.Beds >= 2 ->	The room must have enough beds for all the people.
 
-END
+SELECT dbo.udf_GetAvailableRoom(94, '2015-07-26', 3)
